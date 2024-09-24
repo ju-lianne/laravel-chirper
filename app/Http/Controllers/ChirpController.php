16c16b550c50ca315dea249df49537bff00b6a87
Julianne Duhaze <jduhaze@protonmail.com>
2024-09-24 22:47:46 +0200

    added chirp deletion

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -90,6 +90,8 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        
+        Gate::authorize('delete', $chirp);
+        $chirp->delete();
+        return redirect(route('chirps.index'));
     }
 }
